93a452ed947df0e3bc524dc7d2a900280acb52de
Nueby <yoshinomikoto@vip.qq.com>
2019-10-30 20:58:31 +0800

    ..

--- passerby/src/main/webapp/js/main.js
+++ passerby/src/main/webapp/js/main.js
@@ -204,6 +204,7 @@ document.getElementById("self").onclick = function() {
 }
 
 
+<<<<<<< HEAD
 
 
 //展开修改邮箱或者密码内容
@@ -216,6 +217,8 @@ $("#icon_passwordchange").click(function(){
 	$("#change_passwordmore").hide();
 })
 
+=======
+>>>>>>> parent of b00e900... 10/29/22:56
 
 $id("email_submit").onclick = function() {
 	$("#change_password").hide();
@@ -1616,6 +1619,7 @@ function getNowDate(){
 
 //收邮件的页面显示
 $("#email_main").click(function(){
+<<<<<<< HEAD
 	$("#email_contact").show();
 })
 $("#icon_emailmore").click(function(){
@@ -1640,3 +1644,23 @@ $("#email_back").click(function(){
 	$("#email_content").css({"overflow":"hidden"});
 })
 
+=======
+	
+})
+
+function getNowFormatDate() {
+	var date = new Date();
+	var seperator1 = "-";
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var strDate = date.getDate();
+	if (month >= 1 && month <= 9) {
+		month = "0" + month;
+	}
+	if (strDate >= 0 && strDate <= 9) {
+		strDate = "0" + strDate;
+	}
+	var currentdate = year + seperator1 + month + seperator1 + strDate;
+	return currentdate;
+}
+>>>>>>> parent of b00e900... 10/29/22:56
